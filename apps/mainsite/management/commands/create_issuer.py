a511a10e727d8223fdf7d61b2cff4b733f59c1a7
Ali Salman <ali.salman@A006-01181.local>
2024-01-19 18:06:08 +0500

    fix: issuer job

--- apps/mainsite/management/commands/create_issuer.py
+++ apps/mainsite/management/commands/create_issuer.py
@@ -15,8 +15,12 @@ class Command(BaseCommand):
         """
         Arguments for the mangaement command
         """
-        parser.add_argument('name',
+        parser.add_argument('entity_id',
                             action='store',
+                            help='Issuer Slug')
+        parser.add_argument('--create-name',
+                            action='store',
+                            dest='name',
                             help='Name of the Issuer')
         parser.add_argument('--username',
                             action='store',
@@ -29,10 +33,6 @@ class Command(BaseCommand):
                             action='store',
                             dest='badgrapp_name',
                             help='Name of the badgr app')
-        parser.add_argument('--entity-id',
-                            action='store',
-                            dest='entity_id',
-                            help='Issuer Slug')
         parser.add_argument('--verified',
                             action='store_true',
                             dest='verified',
@@ -42,14 +42,14 @@ class Command(BaseCommand):
                             dest='update',
                             help='If App already exist, update values.')
 
-    def _create_issuer(self, issuer_data):
+    def _create_issuer(self, name, issuer_data):
         """
         Create a new Issuer
         """
         issuer_app = Issuer.objects.create(
-            **issuer_data
+            name=name, **issuer_data
         )
-        self.stdout.write('Created {} Issuer'.format(issuer_app.name))
+        self.stdout.write(f'Created {issuer_app.name} Issuer')
         return issuer_app
 
     def _update_issuer(self, issuer_app, issuer_data):
@@ -59,7 +59,7 @@ class Command(BaseCommand):
         for key, value in issuer_data.items():
             setattr(issuer_app, key, value)
         issuer_app.save()
-        self.stdout.write('Updated {} Issuer'.format(issuer_app.name))
+        self.stdout.write(f'Updated {issuer_app.name} Issuer')
 
     def handle(self, *args, **options):
         """
@@ -67,13 +67,13 @@ class Command(BaseCommand):
         """
         update = options['update']
         username = options['username']
+        name = options['name']
 
         badgrapp = BadgrApp.objects.filter(name=options['badgrapp_name']).first()
-       
+
         issuer_data = dict(
-            name=options['name'],
             url=options['url'],
-            badgrapp = badgrapp,
+            badgrapp=badgrapp,
             entity_id=options['entity_id'],
             verified=options['verified'],
         )
@@ -81,14 +81,14 @@ class Command(BaseCommand):
         if username:
             badgeuser = BadgeUser.objects.filter(username=username).first()
             issuer_data['created_by'] = badgeuser
-        
-        issuer = Issuer.objects.filter(name=options['name']).first()
+
+        issuer = Issuer.objects.filter(entity_id=options['entity_id']).first()
         if issuer and update:
             self._update_issuer(issuer, issuer_data)
             self.stdout.write(self.style.SUCCESS(f'Successfully Updated Issuer with id {issuer.id}'))
         elif issuer:
-            self.stdout.write('The issuer with the given name: {} already exists'.format(issuer.name))
+            self.stdout.write(f'The issuer with the given name: {issuer.name} already exists')
         else:
             # Create a Issuer
-            issuer = self._create_issuer(issuer_data)
+            issuer = self._create_issuer(name, issuer_data)
             self.stdout.write(self.style.SUCCESS(f'Successfully Created Issuer with id {issuer.id}'))
